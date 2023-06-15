19a645ff0546a611ce6cf0020b37705bc490aea9
ohansyah <ohan.ohansyah@gmail.com>
2023-06-15 08:14:13 +0700

    adjust: using translation

--- resources/views/livewire/post-index.blade.php
+++ resources/views/livewire/post-index.blade.php
@@ -15,7 +15,7 @@
             </p>
 
             <a href="{{ route('post.show', ['post' => $post->id]) }}" class="inline-flex items-center font-semibold text-indigo-700 dark:text-indigo-300">
-                Read more
+                {{ __('general.read_more') }}
 
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" class="ml-1 w-5 h-5 fill-indigo-500 dark:fill-indigo-200">
                     <path fill-rule="evenodd" d="M5 10a.75.75 0 01.75-.75h6.638L10.23 7.29a.75.75 0 111.04-1.08l3.5 3.25a.75.75 0 010 1.08l-3.5 3.25a.75.75 0 11-1.04-1.08l2.158-1.96H5.75A.75.75 0 015 10z" clip-rule="evenodd" />
