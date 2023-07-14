93bfc9f623d23dc9dad439addc5c66ed23946a4b
ohansyah <ohan.ohansyah@gmail.com>
2023-07-14 21:51:04 +0700

    adjust: comment - add debounce ( delay on hit livewire component )

--- resources/views/livewire/comment-create.blade.php
+++ resources/views/livewire/comment-create.blade.php
@@ -7,7 +7,7 @@
 
         <div class="form-control">
             <input type="hidden" name="post_id" id="post_id" wire:model="post_id" value="{{ $postId }}">
-            <textarea name="content" id="content" wire:model="content" class="textarea textarea-bordered h-24" placeholder="{{ __('general.placeholder') }}"></textarea>
+            <textarea name="content" id="content" wire:model.debounce.1000ms="content" class="textarea textarea-bordered h-24" placeholder="{{ __('general.placeholder') }}"></textarea>
         </div>
 
         @include('components.form.btn-yes-no', ['route' => 'post.index'])
