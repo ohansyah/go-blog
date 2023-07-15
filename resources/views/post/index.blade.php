f1f86ff2c8b4592fb492e4ce60f9d3add49b67c6
ohansyah <ohan.ohansyah@gmail.com>
2023-07-16 00:50:34 +0700

    adjust: post - index - adjust layout

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -7,30 +7,41 @@
                     <div class="p-6 lg:p-8 bg-white dark:bg-gray-800 dark:bg-gradient-to-bl dark:from-gray-700/50 dark:via-transparent border-b border-gray-200 dark:border-gray-700">
 
                         <a href="{{ route('post.show', ['post' => $post->id]) }}">
-                            <span class="text-gray-500 dark:text-gray-400 text-right text-sm">{{ $post->created_at_format_dMY }}</span>
-                            <div class="flex justify-between items-center">
-                                <h1 class="text-2xl font-medium text-gray-900 dark:text-white">
-                                    {{$post->title}}
-                                </h1>
-                            </div>
-
-                            @if ($post->category_name)
-                                <div class="badge badge-outline">{{ $post->category_name }}</div>
-                            @endif
 
                             @if($post->postImage)
                                 <div class="flex">
                                     <div class="w-2/3">
+                                        <div class="flex justify-between items-center">
+                                            <h1 class="text-2xl font-medium text-gray-900 dark:text-white">
+                                                {{$post->title}}
+                                            </h1>
+                                        </div>
+                                        <span class="text-gray-500 dark:text-gray-400 text-right text-sm">{{ $post->created_at_format_dMY }}</span>
+
+                                        @if ($post->category_name)
+                                            <div class="badge badge-outline">{{ $post->category_name }}</div>
+                                        @endif
+
                                         <article class="prose max-w-screen-2xl dark:prose-invert mt-3">{!! $post->content_preview !!}</article>
                                     </div>
 
                                     <div class="w-1/3 ml-6">
                                         <img src="{{ asset('storage/' . $post->postImage->path) }}" alt="Preview" class="mt-2 max-h-60 object-cover rounded-md mx-auto">
                                     </div>
-
                                 </div>
                             @else
                                 <div class="">
+                                    <div class="flex justify-between items-center">
+                                        <h1 class="text-2xl font-medium text-gray-900 dark:text-white">
+                                            {{$post->title}}
+                                        </h1>
+                                    </div>
+                                    <span class="text-gray-500 dark:text-gray-400 text-right text-sm">{{ $post->created_at_format_dMY }}</span>
+
+                                    @if ($post->category_name)
+                                        <div class="badge badge-outline">{{ $post->category_name }}</div>
+                                    @endif
+
                                     <article class="prose max-w-screen-2xl dark:prose-invert mt-3">{!! $post->content_preview !!}</article>
                                 </div>
                             @endif
