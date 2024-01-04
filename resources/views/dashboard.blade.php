8a0cca1a6a94b28c1dd02fa44b67526d5024e8c5
ohansyah <ohan.ohansyah@gmail.com>
2024-01-04 23:39:11 +0700

    feat: add weather temperature

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,10 @@
 <x-app-layout>
     <div class="py-12">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div class="inline-block bg-white dark:bg-gray-800 overflow-hidden shadow-xl rounded-lg mb-5">
+                <livewire:weather />
+            </div>
+
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl rounded-lg">
                 <livewire:welcome />
             </div>
