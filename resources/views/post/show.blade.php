b984e1352e4a0019d4c800a3327287943466059b
ohansyah <ohan.ohansyah@gmail.com>
2024-01-04 09:26:27 +0700

    fix: add padding show post

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
                 @livewire('post-show', ['post' => $post, 'isUserPost' => $isUserPost])
             </div>
--- resources/views/public/post/show.blade.php
+++ resources/views/public/post/show.blade.php
@@ -1,6 +1,6 @@
 <x-public-layout>
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
                 @livewire('public-post-show', ['post' => $post])
             </div>
