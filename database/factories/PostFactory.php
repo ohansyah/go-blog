c605a01cc8c0fc10a4833790793c1a000e1f80c1
ohansyah <ohan.ohansyah@gmail.com>
2024-01-09 22:58:06 +0700

    feat: factory for Post and UserPost

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Post;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    protected $model = Post::class;
+    
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'category_id' => $this->faker->numberBetween(1, 5),
+            'title' => $this->faker->sentence(),
+            'content' => $this->faker->paragraph(),
+            'slug' => $this->faker->slug(),
+        ];
+    }
+}
